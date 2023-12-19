a18587884d22a9d5af789f8153d53f20396ac3d0
franciscamanquecheo <f.manquecheo01@ufromail.cl>
2023-12-19 09:52:48 -0300

    Se sube base de proyecto

---
+++ examen_endoPDF/src/main/java/com/example/examen_endoPDF/ExamenEndoPdfApplication.java
@@ -0,0 +1,13 @@
+package com.example.examen_endoPDF;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenEndoPdfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamenEndoPdfApplication.class, args);
+	}
+
+}
---
+++ examen_endoPDF/src/test/java/com/example/examen_endoPDF/ExamenEndoPdfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.examen_endoPDF;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamenEndoPdfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
